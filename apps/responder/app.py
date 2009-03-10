445d171d0b994b8f16fa19d9232a6b3016301163
Adam Mckaig <adam.mckaig@gmail.com>
2009-03-10 11:10:56 -0400

    added auto-responder app, to demonstrate django webui integration

---
+++ apps/responder/app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+import rapidsms
+from models import *
+
+class App(rapidsms.app.App):
+    
+    def __init__(self, router):
+        super(rapidsms.app.App, self).__init__()
+        self.name = "Responder"
+
+    def handle(self, msg):
+        for r in Responder.objects.filter(trigger=msg.text):
+            msg.respond(r.response)
