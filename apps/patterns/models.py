cadca6ab1dcb851b3da04686c052f445271d08d8
Cory Zue <czue@dimagi.com>
2009-05-01 18:03:21 +0100

    moving the pattern building logic to a static method in the pattern model.  Adding patterns to roles, and updating the logic to include the patterns in the validation of roles.

--- apps/patterns/models.py
+++ apps/patterns/models.py
@@ -5,6 +5,18 @@ class Pattern(models.Model):
     name = models.CharField(max_length=160)
     regex = models.CharField(max_length=160)
 
+    @staticmethod
+    def join(patterns):
+        '''Joins a set of passed in patterns, assumed to be the result of a 
+           ManyToMany field foreign key from some other object'''
+        # fix any patterns we just or-ed together so they are the kind of or we want
+        # e.g., (\w+)|(\d+) => (\w+|\d+)
+        # TODO figure out how to handle non-captured matches
+        # e.g., (?:\w+)|(\d+) => (?:\w+)|(\d+) and then zip up tokens correctly
+        regex = '|'.join(patterns.values_list('regex', flat=True))
+        return regex.replace(')|(', '|')
+
+
     def __unicode__(self):
         return "%s %s" % (self.name, self.regex)
 
