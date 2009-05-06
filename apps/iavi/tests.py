29037d1b5e278f94e6cfb6286c0d57faa93359bf
Cory Zue <czue@dimagi.com>
2009-05-06 12:49:43 -0400

    adding unit tests for pin failure after 5 attempts and for the kenya tree flow

--- apps/iavi/tests.py
+++ apps/iavi/tests.py
@@ -8,7 +8,7 @@ from apps.reporters.models import Reporter
 
 class TestApp (TestScript):
     apps = (reporters_app.App, App, tree_app.App )
-    fixtures = ["iavi_locations", "uganda_tree", 'test_backend']
+    fixtures = ["iavi_locations", "iavi_trees", 'test_backend']
     
     
     
@@ -115,16 +115,57 @@ class TestApp (TestScript):
         """
         self.runScript(script)
         
-        # register someone with a pin, and make sure only 
-        # the PIN works
-        self._register("accepted_guy", "0002", "1234")
+        # register someone with a pin, and make sure they get in
+        self._register("accepted_guy", "0002", "1238")
         script = """
             accepted_guy > iavi uganda
             accepted_guy < Hello, Please Reply With Your PIN
-            accepted_guy > 1235
+        """
+    def testPinLogic(self):
+        self._register("pin_logic", "0001", "5555")
+        script = """
+            pin_logic > iavi uganda
+            pin_logic < Hello, Please Reply With Your PIN
+            pin_logic > 1234
             # I would prefer this response was improved
-            accepted_guy < "1235" is not a valid answer. You must enter your 4-digit PIN
-            
+            pin_logic < "1234" is not a valid answer. You must enter your 4-digit PIN
+            pin_logic > 1235
+            pin_logic < "1235" is not a valid answer. You must enter your 4-digit PIN
+            # succeed
+            pin_logic > 5555
+            pin_logic < Did you have sex with your main partner in the last 24 hours? 
+        """
+        self.runScript(script)
+        # test that 5 times and you get bounced
+        self._register("pin_logic_2", "0002", "6666")
+        script = """    
+            pin_logic_2 > iavi uganda
+            pin_logic_2 < Hello, Please Reply With Your PIN
+            pin_logic_2 > 1234
+            pin_logic_2 < "1234" is not a valid answer. You must enter your 4-digit PIN
+            pin_logic_2 > 1235
+            pin_logic_2 < "1235" is not a valid answer. You must enter your 4-digit PIN
+            pin_logic_2 > 5555
+            pin_logic_2 < "5555" is not a valid answer. You must enter your 4-digit PIN
+            pin_logic_2 > abcd
+            pin_logic_2 < "abcd" is not a valid answer. You must enter your 4-digit PIN
+            pin_logic_2 > 7777
+            pin_logic_2 < "7777" is not a valid answer. You must enter your 4-digit PIN
+            pin_logic_2 < Sorry, invalid answer 5 times. Your session will now end. Please try again later.
+            # make sure we got bounced and test the other tree
+            pin_logic_2 > iavi kenya
+            pin_logic_2 < Hello, Please Reply With Your PIN
+            pin_logic_2 > 1234
+            pin_logic_2 < "1234" is not a valid answer. You must enter your 4-digit PIN
+            pin_logic_2 > 1235
+            pin_logic_2 < "1235" is not a valid answer. You must enter your 4-digit PIN
+            pin_logic_2 > 5555
+            pin_logic_2 < "5555" is not a valid answer. You must enter your 4-digit PIN
+            pin_logic_2 > abcd
+            pin_logic_2 < "abcd" is not a valid answer. You must enter your 4-digit PIN
+            pin_logic_2 > 7777
+            pin_logic_2 < "7777" is not a valid answer. You must enter your 4-digit PIN
+            pin_logic_2 < Sorry, invalid answer 5 times. Your session will now end. Please try again later.
         """
         self.runScript(script)
     
@@ -144,10 +185,10 @@ class TestApp (TestScript):
             ugb_1 < Questionnaire is complete. Thank you.
         """
         self.runScript(script)
+        
         # and again in another language
         self._register(**{"phone":"ugb_2", "id": "0002", "language":"ug"})
         script = """
-            # base case
             ugb_2 > iavi uganda
             ugb_2 < Ssebo/Nnyabo Yingiza ennamba yo eye'kyaama mu ssimu yo. Era ennamba eyo giwereze ku kompyuta yaffe.
             ugb_2 > 1234
@@ -161,7 +202,37 @@ class TestApp (TestScript):
         """
         self.runScript(script)
         
+    def testKenyaBasic(self):
+        self._register("kenya_1")
+        script = """
+            # base case
+            kenya_1 > iavi kenya
+            kenya_1 < Hello, Please Reply With Your PIN
+            kenya_1 > 1234
+            kenya_1 < How many times did you have sex in the last 24 hours?
+            kenya_1 > 2
+            kenya_1 < Of the number of times that you had sex in the last 24 hours, how many times were condoms used?
+            kenya_1 > 2
+            kenya_1 < Interview is complete. Remember to use a new condom each time you have sex and take your pills as agreed. Thank you
+        """
+        self.runScript(script)
+        
+        # and again in another language
+        self._register(**{"phone":"kenya_2", "id": "0002", "language":"sw"})
+        script = """
+            kenya_2 > iavi kenya
+            kenya_2 < Tafadhali peana jibu ukitumia nambari yako binafsi ya kujitambulisha
+            kenya_2 > 1234
+            kenya_2 < Umefanya mapenzi mara ngapi kwa masaa ishirini na nne iliopita?
+            kenya_2 > 2
+            kenya_2 < Kwa masaa hiyo kumi na nne iliopita, ulitumia mipira ya Kondom mara ngapi ulipofanya mapenzi?
+            kenya_2 > 2
+            kenya_2 < Maswali yamekwisha. Kumbuka kutumia mpira mpya wa kondom kila unavyo fanya mapenzi na kumeza dawa kama ulivyoshauriwa. Asante sana
+        """
+        self.runScript(script)
+    
     def _register(self, phone="55555", id="0001", pin="1234", location= "22", language="En"):
+        """ Register a user, via the test script. """
         script = """
             # base case - everything's all good
             %(phone)s > *#%(language)s#%(location)s#%(id)s#*
