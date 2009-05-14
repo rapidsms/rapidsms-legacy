15f679c915bda6b81e02b0e54843a22f5fc09f00
Cory Zue <czue@dimagi.com>
2009-05-14 10:26:34 -0400

    adding display strings to all the models, and making the dates of a study participant dates instead of datetimes

--- apps/iavi/models.py
+++ apps/iavi/models.py
@@ -19,14 +19,20 @@ class IaviReporter(Reporter):
     @classmethod
     def get_alias(klass, location, study_id):
         return location + "-" + study_id
-
+    
+    def __unicode__(self):
+        return self.connection().identity
+        
 class StudyParticipant(models.Model):
     """ This represents a participant in the IAVI study. """
     reporter = models.ForeignKey(IaviReporter)
-    start_date = models.DateTimeField()
+    start_date = models.DateField()
     # if the end_date is blank the study will go indefinitely
-    end_date = models.DateTimeField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
     notification_time = models.TimeField()
+    
+    def __unicode__(self):
+        return "%s: %s - %s" % (self.reporter, self.start_date, self.end_date)
     
 
 class TestSession(models.Model):
@@ -42,7 +48,9 @@ class TestSession(models.Model):
     tree_session = models.ForeignKey(Session, null=True, blank=True)
     status = models.CharField(max_length=1, choices=TEST_STATUS_TYPES)
     
-
+    def __unicode__(self):
+        return "%s --> %s" % (self.initiator, self.status)
+    
 class Report(models.Model):
     STATUS_TYPES = (
         ('C', 'Canceled'),
@@ -59,6 +67,9 @@ class Report(models.Model):
     @classmethod
     def pending_sessions(klass):
         return klass.objects.filter(completed=None)
+    
+    def __unicode__(self):
+        return "%s: %s (%s)" % (self.reporter, self.started, self.get_status_display())
     
     
 class KenyaReport(Report):
