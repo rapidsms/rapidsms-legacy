d55b35c83b74ccf81d3a8d463d999c54291116b0
Cory Zue <czue@dimagi.com>
2009-07-01 13:13:21 -0400

    adding in a default configuration that works out of the box.  enabled is the reporters app, messaging, the httptester, and an echo that responds to everything you type.  updating the .ini file to have an out of the box httptester and http server.

--- apps/echo/app.py
+++ apps/echo/app.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4
-
 import rapidsms
 import re
 
@@ -8,8 +5,13 @@ class App(rapidsms.app.App):
     prefix = re.compile(r'^echo\s+',re.I)
     def handle(self, message):
         self.debug("got message %s", message.text)
-        if self.prefix.search(message.text):
-            response = self.prefix.sub("",message.text)
-            self.debug("responding with %s", response)
-            message.respond(response)
-	    return True
+        # for purposes of demonstration, have this 
+        # app respond to everything
+        response = "You said: %s" % message.text
+        self.debug("responding with %s", response)
+        message.respond(response)
+#        if self.prefix.search(message.text):
+#            response = self.prefix.sub("",message.text)
+#            self.debug("responding with %s", response)
+#            message.respond(response)
+        return True
