87626b88caa957b80729b373d41f49f26ea6a5c0
Jeff Wishnie <jeff@wishnie.org>
2009-07-15 03:29:22 +0000

    Nodegraph data model app

---
+++ apps/nodegraph/app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+import rapidsms
+
+#
+# This is a pure data-model 'project'
+# 
+# App class does nothing, see models.py for meat.
+#
+
+class App(rapidsms.app.App):
+    pass
