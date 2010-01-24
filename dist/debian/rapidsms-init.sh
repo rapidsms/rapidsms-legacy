7192c0b175cdf04b14f646ec9415908bd3aea16f
Jeff Wishnie <jeff@wishnie.org>
2010-01-24 09:16:13 +0300

    Switched from using 'manage.py' to rapidsms
    Added parameters for choosing which daemons 'start' starts

--- dist/debian/rapidsms-init.sh
+++ dist/debian/rapidsms-init.sh
@@ -4,7 +4,7 @@
 # IMPORTANT: To use, do the folling:
 #
 # 1. Change 'NAME' variable to the name of your project. E.g. "bednets_for_nigeria"
-# 2. Place this file in the TOP-LEVEL of your project, right where 'manage.py' is
+# 2. Place this file in the TOP-LEVEL of your project, right where 'rapidsms' is
 # 3. Link it into /etc/init.d e.g. > ln -s /usr/local/my_project/rapidsms-init.sh /etc/init.d/
 # 4. Add it to the runlevels, on Ubuntu/Debian there is a nice tool to do this for you:
 #    > sudo update-rc.d rapidsms-init.sh defaults
@@ -15,7 +15,7 @@
 #
 
 ### BEGIN INIT INFO
-# Provides:          amatd daemon instance
+# Provides:          rapidsms daemon instance
 # Required-Start:    $all
 # Required-Stop:     $all
 # Default-Start:     2 3 4 5
@@ -31,7 +31,7 @@ WHERE_AM_I=`dirname $ME`
 
 ############### EDIT ME ##################
 NAME="smsforum" # change to your project name
-DAEMON=$WHERE_AM_I/manage.py
+DAEMON=$WHERE_AM_I/rapidsms
 DAEMON_OPTS=""
 RUN_AS=root
 APP_PATH=$WHERE_AM_I
@@ -39,61 +39,74 @@ ROUTER_PID_FILE=/var/run/${NAME}_router.pid
 #WEBSERVER_PID_FILE=/var/run/${NAME}_webs.pid
 WEBSERVER_PORT=8000
 WEBSERVER_IP=127.0.0.1
+
+# By default both router and webserver are started
+# You may turn off one or the other by setting the appropraite
+# value to 0 for off or 1 for on.
+# 
+# Most common use case would be to turn off the webserver
+# because you are running Django in another container like Apache
+# Nginx, or CherryPy
+#
+START_ROUTER=1
+START_WEBSERVER=1
+
 ############### END EDIT ME ##################
 test -x $DAEMON || exit 0
 
 do_start() {
-    echo -n "Starting router for $NAME... "
-    start-stop-daemon -d $APP_PATH -c $RUN_AS --start --background --pidfile $ROUTER_PID_FILE  --make-pidfile --exec $DAEMON -- route $DAEMON_OPTS
-    echo "Router Started"
-    sleep 2
-    echo -n "Starting webserver for $NAME... "
-    start-stop-daemon -d $APP_PATH -c $RUN_AS --start --background --exec $DAEMON -- runserver $WEBSERVER_IP:$WEBSERVER_PORT
-    echo "Webserver Started"
+    if [ "${START_ROUTER}" -eq 1 ] ; then
+        echo -n "Starting router for $NAME... "
+        start-stop-daemon -d $APP_PATH -c $RUN_AS --start --background --pidfile $ROUTER_PID_FILE  --make-pidfile --exec $DAEMON -- route $DAEMON_OPTS
+        echo "Router Started"
+        sleep 2
+    fi
+    
+    if [ "${START_WEBSERVER}" -eq 1 ] ; then
+        echo -n "Starting webserver for $NAME... "
+        start-stop-daemon -d $APP_PATH -c $RUN_AS --start --background --exec $DAEMON -- runserver $WEBSERVER_IP:$WEBSERVER_PORT
+        echo "Webserver Started"
+    fi
 }
 
 hard_stop_runserver() {
-    for i in `ps aux | grep -i "manage.py runserver" | grep -v grep | awk '{print $2}' ` ; do
+    echo "Hard stopping runserver"
+    for i in `ps aux | grep -i "rapidsms runserver" | grep -v grep | awk '{print $2}' ` ; do
         kill -9 $i
     done
-    echo "Hard stopped runserver"
-}
-
-hard_stop_spomsky() {
-    for i in `ps aux | grep -i "spomskyd" | grep -v grep | awk '{print $2}' ` ; do
-        kill -9 $i
-    done
-    rm $SPOMSKY_PID_FILE 2>/dev/null
-    echo "Hard stopped Spomskyd"
 }
 
 hard_stop_router() {
-    for i in `ps aux | grep -i "manage.py route" | grep -v grep | awk '{print $2}' ` ; do
+    echo "Hard stopping router"
+    for i in `ps aux | grep -i "rapidsms route" | grep -v grep | awk '{print $2}' ` ; do
         kill -9 $i
     done
     rm $ROUTER_PID_FILE 2>/dev/null
-    echo "Hard stopped router"
 }
 
 do_hard_restart() {
     do_hard_stop_all
     do_start
 }
-    
+
 do_hard_stop_all() {
     hard_stop_runserver
     hard_stop_router
 }
 
 do_stop() {
-    echo -n "Stopping router for $NAME... "
-    start-stop-daemon --stop --pidfile $ROUTER_PID_FILE
-    rm $ROUTER_PID_FILE 2>/dev/null
-    echo "Router Stopped"
-    sleep 2
-    echo -n "Stopping webserver for $NAME... "
-    hard_stop_runserver
-    echo "Webserver Stopped"
+    if [ "${START_ROUTER}" -eq 1 ] ; then
+        echo -n "Stopping router for $NAME... "
+        start-stop-daemon --stop --pidfile $ROUTER_PID_FILE
+        rm $ROUTER_PID_FILE 2>/dev/null
+        echo "Router Stopped"
+        sleep 2
+    fi
+    if [ "${START_WEBSERVER}" -eq 1 ] ; then
+        echo -n "Stopping webserver for $NAME... "
+        hard_stop_runserver
+        echo "Webserver Stopped"
+    fi
 }
 
 do_restart() {
@@ -104,50 +117,54 @@ do_restart() {
 
 # check on PID's, if not running, restart
 do_check_restart() {
-    for pidf in $ROUTER_PID_FILE ; do
-	if [ -f $pidf ] ; then
-	    pid=`cat $pidf`
-	    if [ ! -e /proc/$pid ] ; then
-		echo "Process for file $pidf not running. Performing hard stop, restart"
-		do_hard_restart
-		return
+    if [ "${START_ROUTER}" -eq 1 ] ; then
+        for pidf in $ROUTER_PID_FILE ; do
+	    if [ -f $pidf ] ; then
+	        pid=`cat $pidf`
+	        if [ ! -e /proc/$pid ] ; then
+		    echo "Process for file $pidf not running. Performing hard stop, restart"
+		    do_hard_restart
+		    return
+	        fi
 	    fi
-	fi
-    done
+        done
+    fi
     
     # now check for runserver
-    webs=`ps aux | grep -i "manage.py runserver" | grep -v grep | wc -l`
-    if [ $webs -lt 2 ] ; then
-	echo "Can't find webserver, doing hard stop, restart"
-	do_hard_restart
+    if [ "${START_WEBSERVER}" -eq 1 ] ; then
+        webs=`ps aux | grep -i "rapidsms runserver" | grep -v grep | wc -l`
+        if [ $webs -lt 2 ] ; then
+	    echo "Can't find webserver, doing hard stop, restart"
+	    do_hard_restart
+        fi
     fi
 }
 
 case "$1" in
-  start)
+    start)
         do_start
         ;;
-
-  stop)
+    
+    stop)
         do_stop
         ;;
-
-  check-restart)
+    
+    check-restart)
 	do_check_restart
 	;;
-
-  hard-stop)
+    
+    hard-stop)
 	do_hard_stop_all
 	;;
-
-  hard-restart)
+    
+    hard-restart)
 	do_hard_restart
 	;;
-  restart|force-reload)
+    restart|force-reload)
 	do_restart
         ;;
-
-  *)
+    
+    *)
         echo "Usage: $ME {start|stop|restart|force-reload|check-restart|hard-stop|hard-restart}" >&2
         exit 1
         ;;
